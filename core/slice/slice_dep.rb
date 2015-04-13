4a88915a4813fd2ce75bbaee2bb7ca13250d3d17
Tom McSweeney <tjmcs@bendbroadband.com>
2015-04-13 14:00:14 -0700

    Removes support for JRuby from Hanlon

--- core/slice/slice_dep.rb
+++ core/slice/slice_dep.rb
@@ -1,5 +1,4 @@
 # this file defines the list of classes to be loaded under slice
-# this is dependency ordering is required for the code to run under compiled model of class loading under jruby
 # here all the load files are maintained in the order of dependency
 #
 # This can be replace if we find a more meaningful method of loading classes
