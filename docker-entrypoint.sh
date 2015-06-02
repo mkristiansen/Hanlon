1ece150afd646cf84bda2617d82c4cf791c7bb5d
Joseph Callen <jcpowermac@gmail.com>
2015-06-02 18:13:27 -0400

    Changed docker-entrypount for PERSIST_MODE

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [ "$PERSIST_MODE" = "cassandra" ]
+if [ "$PERSIST_MODE" = "@cassandra" ]
 then
   cat <<EOF > ${HANLON_WEB_PATH}/config/cassandra_db.conf
 hosts: $CASSANDRA_PORT_9042_TCP_ADDR
@@ -9,7 +9,7 @@ keyspace: 'project_hanlon'
 repl_strategy: $REPL_STRATEGY
 repl_factor: $REPL_FACTOR
 EOF
-  ./hanlon_init -j '{"persist_mode": "@cassandra", "persist_options_file": "cassandra_db.conf", "hanlon_static_path": "'$HANLON_STATIC_PATH'", "hanlon_subnets": "'$HANLON_SUBNETS'", "hanlon_server": "'$DOCKER_HOST'"}'
+  ./hanlon_init -j '{"persist_mode": "'$PERSIST_MODE'", "persist_options_file": "cassandra_db.conf", "hanlon_static_path": "'$HANLON_STATIC_PATH'", "hanlon_subnets": "'$HANLON_SUBNETS'", "hanlon_server": "'$DOCKER_HOST'"}'
 
 else
   ./hanlon_init -j '{"hanlon_static_path": "'$HANLON_STATIC_PATH'", "hanlon_subnets": "'$HANLON_SUBNETS'", "hanlon_server": "'$DOCKER_HOST'", "persist_host": "'$MONGO_PORT_27017_TCP_ADDR'"}'
