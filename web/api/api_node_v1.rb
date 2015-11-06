ad7cec55ecf31cbbdb614752d2e2e2eb6d87b35f
Tom McSweeney <tjmcs@Toms-MacBook-Pro.local>
2015-11-06 14:17:45 -0800

    removes code that (erroneously) converted the UUID for the node (not the SMBIOS UUID but the Hanlon assigned UUID) to an uppercase string

--- web/api/api_node_v1.rb
+++ web/api/api_node_v1.rb
@@ -453,7 +453,7 @@ module Hanlon
               optional :field, type: String, desc: "Name of field to return ('attributes' or 'hardware_id')"
             end
             get do
-              node_uuid = params[:uuid].upcase
+              node_uuid = params[:uuid]
               node = SLICE_REF.get_object("node_with_uuid", :node, node_uuid)
               raise ProjectHanlon::Error::Slice::InvalidUUID, "Cannot Find Node with UUID: [#{node_uuid}]" unless node && (node.class != Array || node.length > 0)
               selected_option = params[:field]
@@ -489,7 +489,7 @@ module Hanlon
                 optional :ipmi_options, type: String, desc: "The IPMI connect options (JSON string)"
               end
               get do
-                node_uuid = params[:uuid].upcase
+                node_uuid = params[:uuid]
                 node = SLICE_REF.get_object("node_with_uuid", :node, node_uuid)
                 raise ProjectHanlon::Error::Slice::InvalidUUID, "Cannot Find Node with UUID: [#{node_uuid}]" unless node && (node.class != Array || node.length > 0)
                 ipmi_args = params.select { |key| ['ipmi_username', 'ipmi_password'].include?(key) }
@@ -522,7 +522,7 @@ module Hanlon
                 optional :ipmi_options, type: String, desc: "The IPMI connect options (JSON string)"
               end
               post do
-                node_uuid = params[:uuid].upcase
+                node_uuid = params[:uuid]
                 node = SLICE_REF.get_object("node_with_uuid", :node, node_uuid)
                 raise ProjectHanlon::Error::Slice::InvalidUUID, "Cannot Find Node with UUID: [#{node_uuid}]" unless node && (node.class != Array || node.length > 0)
                 ipmi_option_string = params[:ipmi_options]
@@ -552,7 +552,7 @@ module Hanlon
                 requires :action, type: String, desc: "The rebinding action to set"
               end
               post do
-                node_uuid = params[:uuid].upcase
+                node_uuid = params[:uuid]
                 node = SLICE_REF.get_object("node_with_uuid", :node, node_uuid)
                 raise ProjectHanlon::Error::Slice::InvalidUUID, "Cannot Find Node with UUID: [#{node_uuid}]" unless node && (node.class != Array || node.length > 0)
                 set_rebinding_action(node, params[:action])
